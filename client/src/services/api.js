ba0e22b979c9e52c93ed7e627bdc72aa1ff6eb10
Saibhargavi2236 <saibhargavigembali2236@gmail.com>
2026-07-10 14:43:45 +0530

    Fix API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://voice-enabled-katha.onrender.com",
+  baseURL: "https://voice-enabled-katha.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
